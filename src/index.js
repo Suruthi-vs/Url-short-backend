45fdccf727a43e9edd73cacf8cdca91b74eb146f
Suruthi-vs <62285669+Suruthi-vs@users.noreply.github.com>
2020-10-28 00:05:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+require('dotenv').config()
 const app = express();
 
 app.use(bodyParser.json());
